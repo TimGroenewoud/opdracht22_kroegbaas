f6ec680db7f226ed2526658b56703e7c50df029e
Tim Groenewoud <74865860+TimGroenewoud@users.noreply.github.com>
2021-05-10 12:24:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const age = 18;
+
+if (age >= 18) {
+
+    console.log ("Welkom u mag naar binnen");
+
+} else if (age == 17){
+
+    console.log ("kom over een jaar maar terug");
+
+} else{
+
+    console.log ("helaas u mag nog niet nier binnen");
+
+}
+
+const isFemale = true;
+
+if (isFemale) {
+
+    console.log ("Welkom dame");
+    
+}
+
+const driverStatus = "Dronken";
+
+if (driverStatus == "Bob") {
+
+    console.log ("U kunt veilig naar huis");
+
+} else {
+
+    console.log("Nog even wachten");
+
+}
+
